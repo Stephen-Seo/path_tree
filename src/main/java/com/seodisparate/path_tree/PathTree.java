5c4e5a1256b8624e56ed3c60f0f7d93b21ae6e21
Stephen Seo <seo.disparate@gmail.com>
2020-08-17 12:51:32 +0900

    Add (un)license

--- src/main/java/com/seodisparate/path_tree/PathTree.java
+++ src/main/java/com/seodisparate/path_tree/PathTree.java
@@ -1,3 +1,28 @@
+// This is free and unencumbered software released into the public domain.
+// 
+// Anyone is free to copy, modify, publish, use, compile, sell, or
+// distribute this software, either in source code form or as a compiled
+// binary, for any purpose, commercial or non-commercial, and by any
+// means.
+// 
+// In jurisdictions that recognize copyright laws, the author or authors
+// of this software dedicate any and all copyright interest in the
+// software to the public domain. We make this dedication for the benefit
+// of the public at large and to the detriment of our heirs and
+// successors. We intend this dedication to be an overt act of
+// relinquishment in perpetuity of all present and future rights to this
+// software under copyright law.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// 
+// For more information, please refer to <https://unlicense.org>
+
 package com.seodisparate.path_tree;
 
 import java.util.Collection;
@@ -48,7 +73,7 @@ public class PathTree implements Serializable {
         public boolean put(String full_path) {
             if(full_path.startsWith(this.full_path)) {
                 String sub = full_path.substring(this.full_path.length());
-                if(sub.startsWith("/")) {
+                while(sub.startsWith("/")) {
                     sub = sub.substring(1);
                 }
                 int index = sub.indexOf('/');
@@ -81,7 +106,7 @@ public class PathTree implements Serializable {
                 return true;
             } else if(full_path.startsWith(this.full_path)) {
                 String sub = full_path.substring(this.full_path.length());
-                if(sub.startsWith("/")) {
+                while(sub.startsWith("/")) {
                     sub = sub.substring(1);
                 }
                 int index = sub.indexOf('/');
@@ -99,7 +124,7 @@ public class PathTree implements Serializable {
         public boolean remove(String full_path) {
             if(full_path.startsWith(this.full_path)) {
                 String sub = full_path.substring(this.full_path.length());
-                if(sub.startsWith("/")) {
+                while(sub.startsWith("/")) {
                     sub = sub.substring(1);
                 }
                 int index = sub.indexOf('/');
